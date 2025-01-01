db627f7d75ff8205cfc7c95653433041bb76de16
mouadHILLALI <mauadtoto001@gmail.com>
2025-01-01 22:31:46 +0100

    MYM-3 created employee module

---
+++ src/app/features/employee/components/employee.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-components',
+  templateUrl: './employee.component.html',
+  styleUrl: './employee.component.css',
+  standalone:false
+})
+export class EmployeeComponent {
+
+}
---
+++ src/app/features/employee/employee-module.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { EmployeeComponent} from './components/employee.component';
+
+
+
+@NgModule({
+  declarations: [EmployeeComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class UserModuleModule { }
