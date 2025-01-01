7d12f308bd9bc0cf1b333c0b4da09491a3c1075b
mouadHILLALI <mauadtoto001@gmail.com>
2025-01-01 22:35:50 +0100

    MYM-4 created employee service

--- src/app/features/employee/employee-module.module.ts
+++ src/app/features/employee/employee-module.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { EmployeeComponent} from './components/employee.component';
+import { EmployeeService } from './service/employeeService.component';
 
 
 
 @NgModule({
   declarations: [EmployeeComponent],
   imports: [
-    CommonModule
-  ]
+    CommonModule 
+  ] , 
+  providers : [EmployeeService]
 })
 export class UserModuleModule { }
